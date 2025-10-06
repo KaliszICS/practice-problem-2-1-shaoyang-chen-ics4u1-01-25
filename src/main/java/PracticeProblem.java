c8f7f785cd49b72dfed6d243a82f00092be8dfc9
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-10-06 14:16:12 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,43 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int find(int[] givenArr, int givenNumber) {
+		int index = 0;
+		while (index < givenArr.length) {
+			if (givenArr[index] == givenNumber) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	public static int findLast(String[] givenArr, String givenString) {
+		int indexLast = givenArr.length - 1;
+		while (indexLast > 0) {
+			if (givenString.equals(givenArr[indexLast])) {
+				return indexLast;
+			}
+			indexLast--;
+		}
+		return -1;
+	}
+	public static int findSecond(char[] givenArr, char character){
+		int index = 0;
+		int counter = 0;
+		while (index < givenArr.length) {
+			if (givenArr[index] == character) {
+				counter++;
+				if (counter == 2) {
+					return index;
+				}
+				if (counter == 1 && index == givenArr.length - 1) {
+					return index;
+				}
+			}
+			index++;
+		}
+		return -1;
+	}
 
 	
 
